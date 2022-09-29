ae54637f68de8824e4d1daa3fc39ff194419e7c0
Iris Bromley <44480862+irisbromley@users.noreply.github.com>
2022-09-29 11:52:06 +0200

    Calculator JS

---
+++ app.js
@@ -0,0 +1,29 @@
+// listen for click of buttons, then check whether it is a number or a key-operator.
+
+let display = document.getElementById('display');
+
+let keys = Array.from(document.getElementsByClassName('key'));
+
+keys.map(key => {
+    key.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
